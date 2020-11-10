947d4fe144efa1381a426f225adf5d3312770de8
Rahil Patel <patelrahil15@gmail.com>
2020-11-09 19:25:25 -0600

    Shiny Dashboard Made

--- PokemonTierList/app.R
+++ PokemonTierList/app.R
@@ -1,48 +1,110 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinythemes)
+library(MASS)
+library(devtools)
+library(ggplot2)
+library(ggord)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
+# Define UI for application
+ui <- fluidPage(theme = shinytheme("united"),
+  navbarPage("Navigation Bar",
+   tabPanel("Welcome",
+            
+   ),             
+   tabPanel("Pokemon Tier List",
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Pokemon Tier List"),
+    
+    # Sidebar Layout 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+           h3("Pre Process Pokemon Data"),
+           sliderInput("tiersID", "How Many Tiers?", min = 3, max = 10, value = 3),
+           h3("Classify Your Pokemon Below:"),
+           numericInput("hpID","Pokemon HP Stat", value = 10),
+           numericInput("attackID","Pokemon Attack Stat", value = 50),
+           numericInput("SPAttackID", "Pokemon SP-Attack Stat", value = 15),
+           numericInput("defenseID","Pokemon Defense Stat", value = 20),
+           numericInput("SPDefenseID", "Pokemon SP-Defense Stat", value = 7),
+           numericInput("speedID", "Pokemon Speed Stat", value = 100),
+           actionButton("Submit", "Classify Pokemon")
         ),
 
-        # Show a plot of the generated distribution
+        # Output 
         mainPanel(
-           plotOutput("distPlot")
+            plotOutput("ldaPlotOutput"),
+            h3("Your Pokemon is in Tier:"),
+            textOutput("ClassifierOutput"),
         )
     )
+   ),
+   tabPanel("Closest Pokemon",
+   
+   )
+  )  
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-    
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # FindTiers takes in number of tiers and the data it wants to put the tiers in
+    FindTiers <- function(numTiers, pokemonDataInput)
+    {
+        maxStat <- max(pokemonDataInput$base_total)
+        minStat <- min(pokemonDataInput$base_total)
+        df = pokemonDataInput
+        section =  (maxStat-minStat)/numTiers;
+        df$tier <- 0
+        for (row in 1:nrow(df)) 
+        {
+            for(tiered in 1:numTiers)
+            {
+                if(df[row,'base_total'] >= (minStat+(section *(tiered-1))) && df[row,'base_total'] < (minStat+(section*tiered)))
+                {
+                    df[row,'tier'] <- numTiers - (tiered - 1);
+                }
+                if(tiered == numTiers){
+                    if(df[row, 'base_total'] > (minStat+(section *(tiered-1)))){
+                        df[row,'tier'] <- numTiers - (tiered - 1);
+                    }
+                }
+            }
+        }
+        return(df)
+        
+    }
+    
+    output$ldaPlotOutput <- renderPlot({
+        # Get our Pokemon Training Data
+        pokemons <- read.csv("https://raw.githubusercontent.com/RahilPatel04/PokemonTierList/main/TrainingPokeData.csv")
+       
+         #Pre Process our Data
+        
+        # Update the pokemon data with the tiers
+        tiers = input$tiersID
+        pokemons = FindTiers(tiers, pokemons)
+        # Take out the name column
+        pokeData <- pokemons[,2:ncol(pokemons)]
+        # run lda on the tier column. Make ldaData global
+        ldaData <<- lda(tier~., pokeData)
+            
+        ggord(ldaData, factor(pokeData$tier))
+        
+    })
+    
+    observeEvent(input$Submit, {
+        # Calculate Base Total
+        baseTotal = input$hpID + input$attackID + input$SPAttackID + input$defenseID + input$SPDefenseID + input$speedID
+        
+        classifyPokemon <- data.frame(hp = input$hpID, attack = input$attackID, sp_attack = input$SPAttackID, defense = input$defenseID, sp_defense = input$SPDefenseID, speed = input$speedID, base_total = baseTotal)
+        print(classifyPokemon)
+        
+        output$ClassifierOutput <- renderText({
+           # predict the ldaData on the pokemon that the user gives us
+           predictionData <- predict(ldaData, classifyPokemon)
+           # Output the class that the pokemon belongs to
+           predictionData$class[1]
+        })
     })
 }
 
--- PokemonTierList/testScript.R
+++ PokemonTierList/testScript.R
@@ -1,17 +1,22 @@
 library(MASS)
 library(devtools)
 library(ggplot2)
-pokemons <- read.csv("https://raw.githubusercontent.com/RahilPatel04/PokemonTierList/main/PokemonStats.csv")
+library(ggord)
+pokemons <- read.csv("https://raw.githubusercontent.com/RahilPatel04/PokemonTierList/main/TrainingPokeData.csv")
+testPokeData <- read.csv("https://raw.githubusercontent.com/RahilPatel04/PokemonTierList/main/TestPokeData.csv")
+
+CompareAccuracy <- read.csv("https://raw.githubusercontent.com/RahilPatel04/PokemonTierList/main/PokemonStats.csv")
 
 #Pre process the data
 
 # Create Tiers
-maxStat <- max(pokemons$base_total)
-minStat <- min(pokemons$base_total)
-df = pokemons
 
-FindTiers <- function(numTiers)
+# FindTiers takes in number of tiers and the data it wants to put the tiers in
+FindTiers <- function(numTiers, pokemonDataInput)
 {
+  maxStat <- max(pokemonDataInput$base_total)
+  minStat <- min(pokemonDataInput$base_total)
+  df = pokemonDataInput
   section =  (maxStat-minStat)/numTiers;
   df$tier <- 0
   for (row in 1:nrow(df)) 
@@ -32,11 +37,15 @@ FindTiers <- function(numTiers)
   return(df)
   
 }
-df = FindTiers(3)
+# Update the pokemon data with the tiers
+tiers = 3
+pokemons = FindTiers(tiers, pokemons)
+CompareAccuracy = FindTiers(tiers, CompareAccuracy)
+
 
 # Take out the name column
-pokeData <- df[,2:ncol(df)]
-
+pokeData <- pokemons[,2:ncol(pokemons)]
+CompareAccuracyData <- CompareAccuracy[,2:ncol(CompareAccuracy)]
 
 # run lda on the tier column
 ldaData <- lda(tier~., pokeData)
@@ -45,16 +54,19 @@ ldaData$scaling
 ldaData$prior
 
 # predict the ldaData on the pokeData that we already have. I think we can put the test data in pokeData place
-predictionData <- predict(ldaData, pokeData)
+predictionData <- predict(ldaData, testPokeData)
 # I think this data is that each point under lda 1 or 2 is calculated by the coefficents of lda 1 and the columns
-predictionData$class
+predictionData$class[1]
  
 # See how accurate our prediction was and maybe tune it?
 # Table gives you: column is what the predictor said it was and row is what its suppose to be
-predictTable <- table(predictionData$class, pokeData[,ncol(pokeData)])
+predictTable <- table(predictionData$class, CompareAccuracyData[1:49,ncol(CompareAccuracyData)])
 predictTable
 
 # Run a plot to show the distribution of them
 str(pokeData$tier)
 ggord(ldaData, factor(pokeData$tier))
+
+# Create a new plot with the new certain pokemon classified
+print(predictionData$class[1])
 
